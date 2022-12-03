ebcc2940cb479db73333dd1924888aa61508dd48
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2022-12-03 18:08:19 -0500

    new deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors    = require('cors');
 const { dbConnect } = require('./config/db');
 const app = express();
-
+/** DEPLOY TO RAILY APP */
 // CONFIGURAR CORS
 app.use( cors());
 
